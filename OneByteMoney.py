e13b392ee884852c13c7f57a08c8d4ac9b3cd783
Daniel Krupiński <danielkrupinski@outlook.com>
2019-11-01 17:02:49 +0100

    Implement OneByteMoney

---
+++ OneByteMoney.py
@@ -0,0 +1,13 @@
+import pymem
+import re
+
+pm = pymem.Pymem("csgo.exe")
+client = pymem.process.module_from_name(pm.process_handle,
+                                        "client_panorama.dll")
+
+clientModule = pm.read_bytes(client.lpBaseOfDll, client.SizeOfImage)
+address = client.lpBaseOfDll + re.search(rb'.\x0C\x5B\x5F\xB8\xFB\xFF\xFF\xFF',
+                                         clientModule).start()
+
+pm.write_uchar(address, 0xEB if pm.read_uchar(address) == 0x75 else 0x75)
+pm.close_process()
